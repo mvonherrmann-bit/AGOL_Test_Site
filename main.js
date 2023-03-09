50eaf92c708230c1474035b3c6b62d567c15e1fe
mvonherrmann-bit <70783680+mvonherrmann-bit@users.noreply.github.com>
2023-03-09 16:46:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,98 @@ require(["esri/config","esri/views/MapView","esri/Map"],
                 const map = new Map({
                   basemap:"arcgis-streets"
                 });
+            
+                  const template = {
+                    title: "Street Name: {StreetName}",
+                    content: [
+                      {
+                        type: "fields", 
+                        fieldInfos: [
+                          {
+                            fieldName: "StreetName",
+                            label: "Street Name"
+                          },
+                          {
+                            fieldName: "StreetSegment",
+                            label: "Street Segment"
+                          },
+                          {
+                            fieldName: "FromStreet",
+                            label: "From Street"
+                          },
+                          {
+                            fieldName: "ToStreet",
+                            label: "To Street"
+                          },
+                          {
+                            fieldName: "Length",
+                            label: "Length",
+                            format: {
+                              digitSeparator: true,
+                              places: 2
+                            }
+                          },
+                          {
+                            fieldName: "Miles",
+                            label: "Miles",
+                            format: {
+                              digitSeparator: true,
+                              places: 2
+                            }
+                          },
+                          {
+                            fieldName: "Width",
+                            label: "Width",
+                            format: {
+                              digitSeparator: true,
+                              places: 2
+                            
+                          }
+                        },
+                          {
+                            fieldName: "SquareYards",
+                            label: "Square Yards",
+                            format: {
+                              digitSeparator: true,
+                              places: 2
+                            }
+                          },
+                          {
+                            fieldName: "BenefitValue",
+                            label: "Benefit Value",
+                            format: {
+                              digitSeparator: true,
+                              places: 2
+                            }
+                          },
+                          {
+                            fieldName: "AdjustedRSR",
+                            label: "Adjusted RSR",
+                            format: {
+                              digitSeparator: true,
+                              places: 2
+                            }
+                          },
+                          {
+                            fieldName: "RepairMethod",
+                            label: "Repair Method"
+                          },
+                          {
+                            fieldName: "Cost",
+                            label: "Cost",
+                            format: {
+                              digitSeparator: true,
+                              places: 2
+                            }
+                          }
+                          
+              
+                          
+                        
+                        ]
+                      }
+                    ]
+                  }
 
                 const RepairCategory = new FeatureLayer({
                     portalItem: {
@@ -28,6 +120,8 @@ require(["esri/config","esri/views/MapView","esri/Map"],
                     }
                   });
                  
+            
+            
 
 
 
